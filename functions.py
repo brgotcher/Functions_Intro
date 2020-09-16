bb1297d7cf2d3ea731d1bdd5fce3c18ca63d863f
brgotcher <brgotcher@gmail.com>
2020-09-16 06:43:15 -0500

    update functions.py

--- functions.py
+++ functions.py
@@ -6,11 +6,29 @@ def multiply(x, y):
 def is_palindrome(string):
     # backwards = string[::-1]
     # return backwards == string
-    return string[::-1] == string
+    return string[::-1].casefold() == string.casefold()
 
 
-word = input("Please enter a word to check: ")
-if is_palindrome(word.casefold()):
-    print("'{}' is a palindrome".format(word))
+def palindrome_sentence(string):
+    slist = []
+    for char in string:
+        if char.isalnum():
+            slist.append(char)
+    alphanum = "".join(slist)
+    return alphanum[::-1].casefold() == alphanum.casefold()
+
+
+
+# word = input("Please enter a word to check: ")
+# if is_palindrome(word):
+#     print("'{}' is a palindrome".format(word))
+# else:
+#     print("'{}' is not a palindrome".format(word))
+
+sentence = input("Please enter a sentence to check: ")
+if palindrome_sentence(sentence):
+    print("'{}' is a palindrome".format(sentence))
 else:
-    print("'{}' is not a palindrome".format(word))
+    print("'{}' is not a palindrome".format(sentence))
+
+
